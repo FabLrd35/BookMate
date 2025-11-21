83c33caec0216b3c78feac430750a00138001c97
FabLrd35 <crocoblock@gmail.com>
2025-11-22 00:51:54 +0100

    Fix TypeScript error in stats-charts

--- src/components/stats-charts.tsx
+++ src/components/stats-charts.tsx
@@ -134,7 +134,7 @@ export function StatsCharts({
                                     cy="50%"
                                     labelLine={false}
                                     label={({ name, percent }) =>
-                                        percent > 0.05 ? `${name} ${(percent * 100).toFixed(0)}%` : ""
+                                        percent && percent > 0.05 ? `${name} ${(percent * 100).toFixed(0)}%` : ""
                                     }
                                     outerRadius={80}
                                     fill="#8884d8"
